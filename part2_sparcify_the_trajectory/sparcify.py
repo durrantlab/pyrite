39976392b5a50938c1eff222dbfb25d177ab0f19
nrajendiran <nir28@pitt.edu>
2016-11-04 15:16:33 -0400

    Updated sparcify.py

--- part2_sparcify_the_trajectory/sparcify.py
+++ part2_sparcify_the_trajectory/sparcify.py
@@ -1,8 +1,9 @@
 import cStringIO as StringIO
 import pymolecule
+import numpy
 
 # Here are some variables that eventually will be user specified.
-sim_pdb_filename = "sim.pdb"
+sim_pdb_filename = "sim_to_test.pdb"
 frame_stride = 2  # Only look at every other frame.
 atom_stride = 5  # Only save every 5th atom.
 
@@ -11,29 +12,45 @@ atom_stride = 5  # Only save every 5th atom.
 # This list will store the frames as pymolecule objects
 pdb_frames = []
 
+current_frame = 1
+first_line = True
+
 # This list stores the atoms in the current frame
 lines_in_this_frame = []
 
 # Go through each of the lines in the multi-frame pdb file
 for line in open(sim_pdb_filename):
-	if line.startswith("END"):
+	# Need to ignore first line?
+	if first_line:
+		first_line = False
+	elif line.startswith("END"):
 		# It's the end of the frame!
-		# Make the pymolecule object.
-		mol = pymolecule.Molecule()
+		if ((current_frame % frame_stride) == 1):
+			# Make the pymolecule object.
+			mol = pymolecule.Molecule()
+			
+			#Select only every certain number of atoms
+			#print "From lines in frame: ", lines_in_this_frame[0:5]
+			pdb_lines = numpy.array(lines_in_this_frame)
+			#print "From all atoms: \n", pdb_lines[0:5]
+			pdb_lines_atoms = pdb_lines[::atom_stride]
+			#print "From selected atoms: \n", pdb_lines_atoms[0:5]
 
-		# Load these pdb lines into that object
-		pdb_lines = StringIO.StringIO("\n".join(lines_in_this_frame))
-		mol.load_pdb_into_using_file_object(pdb_lines, False, False, False)
+			# Load these pdb lines into that object
+			final_pdb_lines = StringIO.StringIO("\n".join(pdb_lines_atoms))
+			mol.load_pdb_into_using_file_object(final_pdb_lines, False, False, False)
 
-		# Save this pymolecule object to our list
-		pdb_frames.append(mol)
+			# Save this pymolecule object to our list
+			pdb_frames.append(mol)
+			print mol
 
-		# Clear the list of lines for this frame
+		# Clear the list of lines for this frame, increment current frame
 		lines_in_this_frame = []
-		print mol
+		current_frame += 1
+	else:
+		# Add this line to the list of lines for this frame
+		lines_in_this_frame.append(line)
 
-	# Add this line to the list of lines for this frame
-	lines_in_this_frame.append(line)
 
 # Ok, so pdb_frames is a list of pymolecule objects, each one corresponding to a frame.
 
@@ -41,13 +58,13 @@ for line in open(sim_pdb_filename):
 
 
 
-# This is how you can access a pymolecule object:
-mol = pymolecule.Molecule()
-coors = mol.get_coordinates()
-# mess with coors
-# Check out numpy slice
-mol.set_coordinates(coors)
+# # This is how you can access a pymolecule object:
+# 	mol = pymolecule.Molecule()
+# 	coors = mol.get_coordinates()
+# # mess with coors
+# # Check out numpy slice
+# 	mol.set_coordinates(coors)
 
-inf = mol.get_atom_information()
-# mess with inf
-mol.set_atom_information(inf)
+# 	inf = mol.get_atom_information()
+# # mess with inf
+# 	mol.set_atom_information(inf)
