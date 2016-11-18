1cff4dca9eb49994eaff94a53e5cf98c5b4825af
nrajendiran <nir28@pitt.edu>
2016-11-18 00:28:06 -0500

    Updated Blender script to add bones (first frame)

--- part2_sparcify_the_trajectory/blender_import_script.py
+++ part2_sparcify_the_trajectory/blender_import_script.py
@@ -4,34 +4,19 @@ import json
 with open('/Users/niveditarajendiran/Documents/University of Pittsburgh/Senior Year/Durrant Lab/Lab Files/labcode/part2_sparcify_the_trajectory/converting_to_json/frame_coordinate_list.json') as json_data:
     coord_list = json.load(json_data)
 
-# print(coord_list) # Prints entire list of coordinate data
-# print(coord_list[0][0]) # [-30.852, -81.458, 365.055] (Coordinates of frame 0, atom 0)
-
-# Creating armature and adding bones (IN PROGRESS)
+# Creating armature and adding bones
 bpy.ops.object.add(type='ARMATURE', enter_editmode=True)
 object = bpy.context.object
-object.name = 'Armature'
+object.name = 'Armatures'
 armature = object.data
 armature.name = 'Frame1'
 
+# Adds bones for frame 0
 for x in range(len(coord_list[0])):
     bone = armature.edit_bones.new('bonename' + str(x))
-    bone.tail = coord_list[0][x]
     bone.head = coord_list[0][x]
-    
-# Blender keeps crashing after adding all bones
-# Try breaking up into multiple armatures
+    tail = coord_list[0][x]
+    tail[2] = tail[2] - 1   # Reduces Z coordinate by 1 for tail to create bone length
+    bone.tail = tail    # Sets tail as modified atom coordinate
 
 bpy.ops.object.mode_set(mode='OBJECT')
-
-
-# vec = mathutils.Vector((0.0, 0.0, 1.0))
-# eul = mathutils.Euler((0.0, math.radians(45.0), 0.0), 'XYZ')
-# bone.tail = mathutils.Vector([0,0,1])
-# x = bpy.data.objects["Cube"]
-# x.location
-# x.location = ([-30.852, -81.458, 365.055])
-
-# x = bpy.data.armatures["Frame1"].bones['bonename2']
-# armature = bpy.data.armatures["Frame1"]
-# x.head = [-30.852, -81.458, 365.055]
