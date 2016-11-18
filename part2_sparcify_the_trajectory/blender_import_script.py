3f924050339220cfede3fe58f0af96860089108c
nrajendiran <nir28@pitt.edu>
2016-11-18 01:55:33 -0500

    Slight changes to Blender script file

--- part2_sparcify_the_trajectory/blender_import_script.py
+++ part2_sparcify_the_trajectory/blender_import_script.py
@@ -1,19 +1,20 @@
 import json
+import bpy
 
 # Loading JSON data into a list in Blender
 with open('/Users/niveditarajendiran/Documents/University of Pittsburgh/Senior Year/Durrant Lab/Lab Files/labcode/part2_sparcify_the_trajectory/converting_to_json/frame_coordinate_list.json') as json_data:
     coord_list = json.load(json_data)
 
-# Creating armature and adding bones
+# Creating armature
 bpy.ops.object.add(type='ARMATURE', enter_editmode=True)
 object = bpy.context.object
-object.name = 'Armatures'
+object.name = 'Armature'
 armature = object.data
 armature.name = 'Frame1'
 
-# Adds bones for frame 0
+# Adding bones for frame 0
 for x in range(len(coord_list[0])):
-    bone = armature.edit_bones.new('bonename' + str(x))
+    bone = armature.edit_bones.new('atom' + str(x))
     bone.head = coord_list[0][x]
     tail = coord_list[0][x]
     tail[2] = tail[2] - 1   # Reduces Z coordinate by 1 for tail to create bone length
