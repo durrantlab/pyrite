af65447573d263e2a410c1aebd1329dd992dbd55
jdurrant <jacobdurrant@gmail.com>
2017-01-10 21:26:18 -0500

    Path update for Jacob

--- part2_sparcify_the_trajectory/blender_import_script.py
+++ part2_sparcify_the_trajectory/blender_import_script.py
@@ -2,7 +2,7 @@ import json
 import bpy
 
 # Loading JSON data into a list in Blender
-with open('/Users/niveditarajendiran/Documents/University of Pittsburgh/Senior Year/Durrant Lab/Lab Files/labcode/part2_sparcify_the_trajectory/converting_to_json/frame_coordinate_list.json') as json_data:
+with open('/Users/jdurrant/Documents/Work/durrant_git/nivedita/part2_sparcify_the_trajectory/converting_to_json/frame_coordinate_list.json') as json_data:
     coord_list = json.load(json_data)
 
 # Creating armature
