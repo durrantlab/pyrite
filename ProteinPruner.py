e5299a21658b3f2a3e06d27733488a53027a5bde
nrajendiran <nir28@pitt.edu>
2017-01-17 21:54:31 -0500

    Started protein pruner script

---
+++ ProteinPruner.py
@@ -0,0 +1,10 @@
+
+# Hard code input for spheres
+# Find all points in a sphere
+# Select all bones in a sphere
+# Keep only every certain number of bones in the sphere (delete others)
+
+spheres = [
+    # [x, y, z, radius, every_other]
+    [0, 0, 0, 25, 10]
+]
