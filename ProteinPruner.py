9b5e41e2e2b0b1d04cf0770164072cbe1a36810d
nrajendiran <nir28@pitt.edu>
2017-01-24 13:44:22 -0500

    Formatting changes, selects bones to prune

--- ProteinPruner.py
+++ ProteinPruner.py
@@ -7,37 +7,45 @@
 import mathutils
 from bpy import context
 
-# Hard code input for spheres
+# Hard code input for spheres, [x, y, z, radius, every_other]
 spheres = [
-    # [x, y, z, radius, every_other]
     [0, 0, 0, 25, 10],
-    [-30, -70, 375, 25, 100],
+    [-30, -70, 375, 100, 2],
+    [-35, -80, 370, 100, 2],
+    [-35, -80, 370, 10, 2],
 ]
 
 # Create kd-tree containing current bone coordinates
-obj = context.object
-mesh = obj.data
-# size = len(mesh.vertices) # Mesh is an armature, doesn't have vertices attribute
-size = len(bpy.context.visible_bones)   # Tree size is current number of bones
+size = len(bpy.context.visible_bones)
 kd = mathutils.kdtree.KDTree(size)
 bone_list = []
 
+# Compiles list of current bone coordinates
 for i in range(size):
     bone_list.append(bpy.context.visible_bones[0].head)
 
+# Adds coordinates to tree
 for i, v in enumerate(bone_list):
     kd.insert(v, i)
 
 kd.balance()
 
-# Find all bones within radius
-co_find = (-30, -70, 375)
-for (co, index, dist) in kd.find_range(co_find, 100):
-    print(" hi ", co, index, dist)
-
-# Keep only every certain number of bones
+# Find all bones within radius, keep only every certain number of bones
 for x in spheres:
     co_find = (x[0], x[1], x[2])
     radius = x[3]
     every_other = x[4]
-    print(kd.find_range(co_find, radius))
+
+    # print(kd.find_range(co_find, radius))
+    bones_in_sphere = kd.find_range(co_find, radius)
+    bones_to_prune = []
+
+    # Compile list of bones to prune
+    for i, b in enumerate(bones_in_sphere):
+        if (i % every_other) != 0:
+            bones_to_prune.append(b)
+    
+    # Select and remove bones in bones_to_prune
+    for i, b in enumerate(bones_to_prune):
+        
+    # bpy.context.visible_bones[0].name
