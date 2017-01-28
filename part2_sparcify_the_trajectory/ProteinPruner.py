b5806819688228e731998635e1df712a43b4933f
nrajendiran <nir28@pitt.edu>
2017-01-28 01:04:54 -0500

    Finished basic pruning script for spheres

---
+++ part2_sparcify_the_trajectory/ProteinPruner.py
@@ -0,0 +1,43 @@
+import scoria
+import numpy as np
+import mathutils
+from bpy import context
+
+# Hard code input for spheres, [x, y, z, radius, every_other]
+spheres = [
+    [0, 0, 0, 25, 10],
+    [-30, -70, 375, 100, 2],
+    [-35, -80, 370, 100, 2],
+    [-35, -80, 370, 10, 2],
+]
+
+# Load the trajectory
+traj = scoria.Molecule()
+traj.load_pdb_trajectory_into("M2_traj.pdb", bonds_by_distance = False, serial_reindex = False, resseq_reindex = False)
+
+coordinates = traj.get_coordinates()    # Stored as numpy array
+
+# Create kd-tree containing atom/bone coordinates
+size = len(coordinates)
+kd = mathutils.kdtree.KDTree(size)
+bone_list = []
+
+# Add coordinates to tree
+for i, c in enumerate(coordinates):
+    kd.insert(c, i)
+
+kd.balance()
+
+new_spheres = []    # Will contain coordinates for pruned spheres
+
+# Find all bones within radius, keep only every certain number of bones
+for x in spheres:
+    co_find = (x[0], x[1], x[2])
+    radius = x[3]
+    every_other = x[4]
+    bones_in_sphere = kd.find_range(co_find, radius)
+    bones_to_keep = []
+    for i, b in enumerate(bones_in_sphere):
+        if (i % every_other) == 0:
+            bones_to_keep.append(b)
+    new_spheres.append(bones_to_keep)
