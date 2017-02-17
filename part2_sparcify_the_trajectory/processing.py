b8df2133bacfaa4a86f42980add621e1ccd8f9e9
jdurrant <jacobdurrant@gmail.com>
2017-02-17 12:44:57 -0500

    Incorporating latest into processing.py

--- part2_sparcify_the_trajectory/processing.py
+++ part2_sparcify_the_trajectory/processing.py
@@ -1,79 +1,173 @@
-import cStringIO as StringIO
+try: import cStringIO as StringIO
+except: from io import StringIO
+
 import scoria
 import numpy
 import json
+import mathutils
+from bpy import context
+import bpy
 
-import _trkr  # Please leave this intact for now.
-_trkr.trkr()
+#import _trkr  # Please leave this intact for now.
+#_trkr.trkr()
 
 class Mineral:
     def __init__(self):
-        self.pdb_filename = ""
-        self.frame_stride = 1
-        self.atom_stride = 1
-        self.sparce_pdb_txt_frames = []
-        self.json_rep = ""
-        
+        print("Creating mineral object!")
+        self.trajectory = None
+        self.kdtree = None
+        self.overall_pruning_stride = 1
+        self.pruning_spheres = []
+        self.frame_stride = None
 
-    def load_pdb_trajectory(self, pdb_filename, frame_stride, atom_stride):
+    def add_overall_pruning_stride(self, atom_stride):
+        print("Keeping only every " + str(atom_stride) + " atoms...")
+        self.pruning_spheres.append((atom_stride, 0.0, 0.0, 0.0, 1e50))
+        
+    def add_pruning_sphere(self, center_x, center_y, center_z, radius, atom_stride):
+        self.pruning_spheres.append((atom_stride, center_x, center_y, center_z, radius))
+
+    def apply_prune(self):
+        # The key is to use the smallest pruning stride possible for a given
+        # point.
+
+        # Make a kd tree if needed
+        coors = self.trajectory.get_coordinates(frame=0)  # So kdtree
+                                                          # calculated on
+                                                          # coordinates of
+                                                          # first frame only.
+        if self.kdtree is None:
+            # Create kd-tree containing atom/bone coordinates
+            self.kdtree = mathutils.kdtree.KDTree(len(coors))
+            bone_list = []
+
+            # Add coordinates to tree
+            for i, c in enumerate(coors):
+                self.kdtree.insert(c, i)
+
+            self.kdtree.balance()
+        
+        # Make sure the pruning spheres are ordered by the stride, from
+        # smallest to greatest.
+        self.pruning_spheres.sort()
+
+        # Go through each sphere and apply a mask, where true means the
+        # coordinate is in the given sphere, and false means it isn't.
+        masks = []
+        for sphere in self.pruning_spheres:
+            # Get the coordinate indecies that are in the sphere
+            atom_stride, center_x, center_y, center_z, radius = sphere
+            co_find = (center_x, center_y, center_z)
+            coors_in_sphere = self.kdtree.find_range(co_find, radius)
+            coors_in_sphere = numpy.array(coors_in_sphere)
+
+            # doesn't work.
+            indecies_in = set([])
+            for coor in coors_in_sphere:
+                indecies_in.add(int(coor[1]))
+
+            # Make the mask, with those set to true that are within the
+            # sphere.
+            mask = numpy.zeros(self.trajectory.get_total_number_of_atoms()).astype(bool)
+            indecies_in = list(indecies_in)
+
+            if len(indecies_in) > 0:
+                mask[indecies_in] = True
+
+            # Save that mask
+            masks.append(mask)
+        
+        # Now go through each of the points and decide whether or not to keep
+        # it.
+        indecies_to_keep = []
+        for coor_index, coor in enumerate(coors):
+            # Find the sphere that this point is in with the lowest stride.
+            # Use that stride.
+            for sphere_index, sphere in enumerate(self.pruning_spheres):
+                if masks[sphere_index][coor_index] == True:
+                    # The point is in one of the spheres.
+                    atom_stride = self.pruning_spheres[sphere_index][0]
+                    if coor_index % atom_stride == 0:
+                        # It matches the stride, so keep it.
+                        indecies_to_keep.append(coor_index)
+                    break  # No need to keep looking through the spheres for
+                           # this point. You've got your match.
+        
+        # Actually prune the molecule.
+        self.trajectory = self.trajectory.get_molecule_from_selection(indecies_to_keep)
+
+    def load_pdb_trajectory(self, pdb_filename, frame_stride = 1): #, atom_stride):
+        print("Loading PDB trajectory: " + pdb_filename)
         self.pdb_filename = pdb_filename
         self.frame_stride = frame_stride
-        self.atom_stride = atom_stride
-
-        # # Here are some variables that eventually will be user specified.
-        # sim_pdb_filename = "M2_traj.pdb"
-        # frame_stride = 2  # Only look at every other frame.
-        # atom_stride = 50  # Only save every 5th atom.
 
         # Load the trajectory
-        traj = scoria.Molecule()
-        traj.load_pdb_trajectory_into(self.pdb_filename, bonds_by_distance = False, serial_reindex = False, resseq_reindex = False)
+        self.trajectory = scoria.Molecule()
+        self.trajectory.load_pdb_trajectory_into(self.pdb_filename, bonds_by_distance = False, serial_reindex = False, resseq_reindex = False)
 
-        # Select every few atoms for now. But when we have the code to make certain regions denser, this will get more complicated.
-        sel = traj.select_all()[::atom_stride]
+        # Delete every frame_stride frames.
+        print("Keeping only every " + str(frame_stride) + " frames...")
+        frame_indecies = numpy.array(range(self.trajectory.get_trajectory_frame_count()))
+        frame_indecies_to_keep = frame_indecies[::frame_stride]
+        frame_indecies_to_delete = numpy.setdiff1d(frame_indecies, frame_indecies_to_keep)
+        for idx in frame_indecies_to_delete[::-1]:
+            self.trajectory.delete_trajectory_frame(idx)
 
-        # Make a new molecule object from that selection, given the current frame.
-        pruned_traj = traj.get_molecule_from_selection(sel, serial_reindex = False, resseq_reindex = False)
+    def make_bones_from_molecues(self):
+        try:  # So dumb that blender throws an error if it's already in object mode...
+            bpy.ops.object.mode_set(mode='OBJECT')
+        except:
+            pass
 
-        # Save only every few frames (the pdb txt)
-        for frame_index in range(0, traj.get_trajectory_frame_count(), self.frame_stride):
-            txt = pruned_traj.save_pdb("", False, False, True, frame_index)
-            self.sparce_pdb_txt_frames.append(txt)
-    
-    def make_json_representation(self):
-        frames_list = []    # Will store lists of coordinate lists
+        # Add enough empties to match the number of bones. 
+        for index in range(self.trajectory.get_total_number_of_atoms()):
+            empty = bpy.ops.object.empty_add(type='PLAIN_AXES', radius=1)
+            empty = bpy.context.object
+            empty.name = "empty" + str(index)
 
-        # Go through each single-frame pdb file
-        for x in range(len(self.sparce_pdb_txt_frames)):
-            coordinate_list = []    # Will store lists of coordinates
+        # Now go through the frames and position those empties
+        for frame_index in range(0, self.trajectory.get_trajectory_frame_count(), self.frame_stride):
+            bpy.context.scene.frame_set(frame_index)  # Sets next frame to add
 
-            txt = self.sparce_pdb_txt_frames[x].strip().split("\n")
-            # Go through each of the lines in a single-frame pdb file
-            for line in txt:
-                # Obtain x, y, z coordinate elements from line if not empty
-                if line != '\n':
-                    coordinates = [line[30:38]]       # x
-                    coordinates.append(line[38:46])   # y
-                    coordinates.append(line[46:54])   # z
-                    coordinates = [float(coordinates[0].strip()), float(coordinates[1].strip()), float(coordinates[2].strip())]
+            for coor_index, coor in enumerate(self.trajectory.get_coordinates(frame=frame_index)):
+                empty = bpy.data.objects["empty" + str(coor_index)]
+                empty.location = coor
+                empty.keyframe_insert(data_path='location')
 
-                    coordinate_list.append(coordinates)
+        # Creating armature
+        bpy.ops.object.add(type='ARMATURE', enter_editmode=True)
+        object = bpy.context.object
+        object.name = 'Armature'
+        armature = object.data
+        armature.name = 'Frame'
 
-                    # 0, 1, 2
-                    ## 0, 2, 1
-                    ## 1, 0, 2
-            # Append list of coordinate lists from newly read frame to the frame list
-            frames_list.append(coordinate_list)
+        # Add bones
+        for index in range(self.trajectory.get_total_number_of_atoms()):
+            bone_name = 'bone' + str(index)
+            bone = armature.edit_bones.new(bone_name)
+            bone.head = (0,0,0)
+            bone.tail = (0,0,2)
+            #bone.envelope_weight = 1.0  # Needed for envelope-based mesh vertex weighting.
+            #bone.envelope_distance = 2.0
+        
+        # Now constrain them.
+        bpy.ops.object.mode_set(mode='POSE')
+        armature = bpy.data.objects["Armature"]
 
-        # Write and save coordinate data in json file
-        # In the future, everything will be in one big class:
-        # self.json_rep =json.dumps(frames_list)
-
-        # For now, you need a way of loading into blender, so make a separate json file.
-        with open('frame_coordinate_list.json', 'w') as f:
-            json.dump(frames_list, f)        
+        for index in range(self.trajectory.get_total_number_of_atoms()):
+            bone = armature.pose.bones['bone' + str(index)]
+            constraint = bone.constraints.new(type="COPY_LOCATION")
+            constraint.target = bpy.data.objects["empty" + str(index)]
+        
+        # Now make sure the pose at frame 0 is set as the rest pose (so you
+        # can do automatic weights later...)
+        bpy.context.scene.frame_set(0)
+        bpy.ops.pose.armature_apply()
 
     
 mineral = Mineral()
-mineral.load_pdb_trajectory("M2_traj.pdb", 2, 50)
-mineral.make_json_representation()
+mineral.load_pdb_trajectory("M2_traj.pdb", 2)
+mineral.add_overall_pruning_stride(15)
+mineral.add_pruning_sphere(-30, -85, 397, 8, 1)
+mineral.apply_prune()
+mineral.make_bones_from_molecues()
